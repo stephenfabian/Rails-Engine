4182b9299c7b64256d57f65b96e79a5f4ae7a8d1
Stephen Fabian <fabianstephen@gmail.com>
2022-10-18 13:52:39 -0600

    adds tests and feature for get one item

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::MerchantItemsController < ApplicationController
 
   def index
     merchant = Merchant.find(params[:merchant_id])
-    render json: ItemsSerializer.new(merchant.items)
+    render json: ItemSerializer.new(merchant.items)
   end
 end
